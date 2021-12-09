2e5486411baa249eed0dbb5c7ea4adb8c77e65fd
Alex Sayers <alex@asayers.com>
2021-12-09 19:01:16 +0900

    Better panic message if nulls slip through to from_sqlite()

--- src/conversion.rs
+++ src/conversion.rs
@@ -9,6 +9,7 @@ impl FromSqlite for bool {
     fn from_sqlite(x: Value) -> Self {
         match x {
             Value::Integer(x) => x == 1,
+            Value::Null => unreachable!("Nulls are handled separately"),
             _ => panic!(),
         }
     }
@@ -17,6 +18,7 @@ impl FromSqlite for i32 {
     fn from_sqlite(x: Value) -> Self {
         match x {
             Value::Integer(x) => i32::try_from(x).unwrap(),
+            Value::Null => unreachable!("Nulls are handled separately"),
             _ => panic!(),
         }
     }
@@ -25,6 +27,7 @@ impl FromSqlite for i64 {
     fn from_sqlite(x: Value) -> Self {
         match x {
             Value::Integer(x) => i64::try_from(x).unwrap(),
+            Value::Null => unreachable!("Nulls are handled separately"),
             _ => panic!(),
         }
     }
@@ -33,6 +36,7 @@ impl FromSqlite for Int96 {
     fn from_sqlite(x: Value) -> Self {
         match x {
             Value::Integer(_) => todo!(),
+            Value::Null => unreachable!("Nulls are handled separately"),
             _ => panic!(),
         }
     }
@@ -41,6 +45,7 @@ impl FromSqlite for f32 {
     fn from_sqlite(x: Value) -> Self {
         match x {
             Value::Real(x) => x as f32,
+            Value::Null => unreachable!("Nulls are handled separately"),
             _ => panic!(),
         }
     }
@@ -49,6 +54,7 @@ impl FromSqlite for f64 {
     fn from_sqlite(x: Value) -> Self {
         match x {
             Value::Real(x) => x,
+            Value::Null => unreachable!("Nulls are handled separately"),
             _ => panic!(),
         }
     }
@@ -58,6 +64,7 @@ impl FromSqlite for ByteArray {
         match x {
             Value::Text(x) => ByteArray::from(Vec::from(x)),
             Value::Blob(x) => ByteArray::from(x),
+            Value::Null => unreachable!("Nulls are handled separately"),
             _ => panic!(),
         }
     }
@@ -67,6 +74,7 @@ impl FromSqlite for FixedLenByteArray {
         match x {
             Value::Text(_) => todo!(),
             Value::Blob(_) => todo!(),
+            Value::Null => unreachable!("Nulls are handled separately"),
             _ => panic!(),
         }
     }
